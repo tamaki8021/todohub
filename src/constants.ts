5e66e58b149097eb92d924c6dafff1bcc1d0c599
tamaki <hbk2002320@gmail.com>
2021-02-22 22:21:29 +0900

    feat/filter-const

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+export const VISIBILITY_FILTERS = {
+  ALL: "all",
+  COMPLETED: "completed",
+  INCOMPLETE: "incomplete"
+} as const;
+
+// js version
+// export const VISIBILITY_FILTERS = {
+//   readonly ALL: "all";
+//   readonly COMPLETED: "completed";
+//   readonly INCOMPLETE: "incomplete";
+// }
