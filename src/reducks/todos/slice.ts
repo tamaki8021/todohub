0ff938fb6a42f5acbb2e41de6484f632a9b9de4a
tamaki <hbk2002320@gmail.com>
2021-02-24 14:44:03 +0900

    feat/edit-todo

--- src/reducks/todos/slice.ts
+++ src/reducks/todos/slice.ts
@@ -1,5 +1,5 @@
 import { createSlice, nanoid, PayloadAction } from "@reduxjs/toolkit"
-import { TodoState, TodoItemState } from "./types"
+import { TodoState, TodoItemState, TodoItem } from "./types"
 
 
 const initialState: TodoState = {
@@ -28,14 +28,26 @@ const todosSlice = createSlice({
     toggleTodo: (state: TodoState, action: PayloadAction<{id: string}>) => {
       const byIds = state.byIds
 
-      const todo = state.allIds.find((v) => v === action.payload.id)
+      const todo = state.allIds.find((todo) => todo === action.payload.id)
       if (todo) {
-        byIds[todo].completed = !byIds[todo].completed
+        byIds.todo.completed = !byIds.todo.completed
+      }
+    },
+    editTodo: (state: TodoState, action: PayloadAction<TodoItem>) => {
+      const byIds = state.byIds
+      const { id, contents, completed } = action.payload
+      
+      const todoId = state.allIds.find((todo) => todo === id)
+
+      if (todoId) {
+        const editData = state.allIds.map(data => data === id ? {contents, completed} : state.byIds.todoId)
+
+        byIds[todoId] = editData[0]
       }
     }
   }
 })
 
-export const { addTodo, toggleTodo } = todosSlice.actions
+export const { addTodo, toggleTodo, editTodo } = todosSlice.actions
 
 export default todosSlice.reducer
