97741afe4ed50436a1578f62e8698fb1622ad5b8
tamaki <hbk2002320@gmail.com>
2021-02-25 20:25:56 +0900

    feat/firebase-conect

---
+++ src/firebase/index.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/functions'
+import { firebaseConfig } from './config'
+
+// initialSizeを複数回走らせない
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth()
+export const db = firebase.database()
+export const functions = firebase.functions()
+export const FirebaseTimestamp = firebase.firestore.Timestamp
