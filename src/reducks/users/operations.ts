e0569c6270222b60713d744342b4ac218d4a494e
tamaki <hbk2002320@gmail.com>
2021-02-27 13:45:42 +0900

    yet/file-custom

--- src/reducks/users/operations.ts
+++ src/reducks/users/operations.ts
@@ -1,44 +1,86 @@
-import { push } from 'connected-react-router'
-import { auth, db, FirebaseTimestamp } from '../../firebase/index'
+import { push } from "connected-react-router";
+import { auth, db, FirebaseTimestamp } from "../../firebase/index";
+import { signInAction } from "./slice";
 
-type userData = {
-  username: string
-  email: string
-  password: string
+export const signUp = (
+  username: string,
+  email: string,
+  password: string,
   confiramPassword: string
-}
-
-
-export const signUp = (username: string, email: string, password: string, confiramPassword: string) => {
+) => {
   return async (dispatch: any) => {
-    if (username === "" || email === "" || password === "" || confiramPassword === "") {
-      alert('必須項目が未入力です')
-      return false
+    if (
+      username === "" ||
+      email === "" ||
+      password === "" ||
+      confiramPassword === ""
+    ) {
+      alert("必須項目が未入力です");
+      return false;
     }
 
     if (password !== confiramPassword) {
-      alert('パスワードが一致しません。もう一度お試しください')
-      return false
+      alert("パスワードが一致しません。もう一度お試しください");
+      return false;
     }
-    
-    return auth.createUserWithEmailAndPassword(email, password).then(result => {
-      const user = result.user
 
-      if (user) {
-        const uid = user.uid
-        const timestamp = FirebaseTimestamp.now()
+    return auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        const user = result.user;
 
-        const userInitialData = {
-          created_at: timestamp,
-          email: email,
-          role: "customer",
-          uid: uid,
-          updated_at: timestamp,
-          username: username
+        if (user) {
+          const uid = user.uid;
+          const timestamp = FirebaseTimestamp.now();
+
+          const userInitialData = {
+            created_at: timestamp,
+            email: email,
+            role: "customer",
+            uid: uid,
+            updated_at: timestamp,
+            username: username,
+          };
+
+          db.collection("users")
+            .doc(uid)
+            .set(userInitialData)
+            .then(() => dispatch(push("/todo")));
         }
+      });
+  };
+};
 
-        db.collection("user").doc(uid).set(userInitialData).then(() => dispatch(push("/")))
-      }
-    })
-  }
-}
+// export const signIn = (email: string, password: string) => {
+//   return async (dispatch: any) => {
+//     if (email === "" || password === "") {
+//       alert("必須項目が未入力です");
+//       return false;
+//     }
+
+//     await auth.signInWithEmailAndPassword(email, password).then((result) => {
+//       const user = result.user;
+
+//       if (user) {
+//         const uid = user.uid;
+//         db.collection("users")
+//           .doc(uid)
+//           .get()
+//           .then((snapshot) => {
+//             const data = snapshot.data();
+
+//             dispatch(
+//               signInAction({
+//                 isSignedIn: true,
+//                 role: data.role,
+//                 uid: uid,
+//                 username: data.username,
+//               })
+//             );
+
+//             dispatch(push("/todo"));
+//           });
+//       }
+//     });
+//   };
+// };
