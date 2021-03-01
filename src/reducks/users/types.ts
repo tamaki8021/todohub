1aa830e86b57ce42eb398b672bc1c5e1f53cb503
tamaki <hbk2002320@gmail.com>
2021-03-01 23:57:33 +0900

    feat/type

--- src/reducks/users/types.ts
+++ src/reducks/users/types.ts
@@ -2,5 +2,15 @@ export interface UserState{
   isSignedIn: boolean,
   role: string,
   uid: string,
-  username: string
+  username: string,
+
 }
+
+  // email: string,
+  // created_at: UserTimeState,
+  // updated_at: UserTimeState,
+
+// export interface UserTimeState{
+//   nanoseconds: number,
+//   seconds: number
+// }
