22c6b75696ec709bb3011bcda8860a88893e36c9
tamaki <hbk2002320@gmail.com>
2021-03-01 23:56:28 +0900

    feat/signin-page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
-import {Home, SignUp, Todo } from './pages/index'
+import {Home, SignUp, SignIn, Todo } from './pages/index'
 
 const Router = () => {
   return (
     <Switch>
       <Route exact path="(/)?" component={Home} />
-      <Route exact path="/login" component={SignUp} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/login" component={SignIn} />
       <Route exact path="/todo" component={Todo} />
     </Switch>
   )
