d4cb88321add257cedf6cd9925921539eb2f0501
tamaki <hbk2002320@gmail.com>
2021-03-07 17:17:46 +0900

    feat/header-menu-components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,46 +3,36 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import { useAppDispatch, useAppSelector } from "../../reducks/store/hooks";
-import { push } from 'connected-react-router'
-import { HeaderMenu } from './index'
+import { Link } from "react-router-dom";
+import { useAppSelector } from "../../reducks/store/hooks";
+import { HeaderMenu, ClosableDrawer } from "./index";
+
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
     title: {
       flexGrow: 1,
+      color: "white",
+      textDecoration: "none",
     },
   })
 );
 
 const Header = () => {
   const classes = useStyles();
-  const dispatch = useAppDispatch();
   const isSignedIn = useAppSelector((state) => state.user.isSignedIn);
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar>
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          {isSignedIn ? (
-            <HeaderMenu />
-          ):(
-            <div>
-              <Button color='inherit' onClick={() => dispatch(push('/signin'))}>ログイン</Button>
-              <Button color='inherit' onClick={() => dispatch(push('/signup'))} >登録する</Button>
-            </div>
-          )
-          }
+          <Link to="/" className={classes.title}>
+            <Typography variant="h6">Todo Motivation</Typography>
+          </Link>
+          {isSignedIn ? <ClosableDrawer /> : <HeaderMenu />}
         </Toolbar>
       </AppBar>
     </div>
