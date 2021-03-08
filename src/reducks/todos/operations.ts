9955c374de139d826a91121b0b5a58212f96a361
tamaki <hbk2002320@gmail.com>
2021-03-08 18:04:21 +0900

    feat

---
+++ src/reducks/todos/operations.ts
@@ -0,0 +1,10 @@
+// import { db } from '../../firebase'
+
+
+
+// export const createTodo = (addTodo) => {
+//   return async (dispatch, getState) => {
+//     const uid = getState()
+//     await db.collection("users").doc()
+//   }
+// }
