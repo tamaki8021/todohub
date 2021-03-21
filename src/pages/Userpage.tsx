6f2b25225b861cda50892d070ba20df7f808df38
tamaki <hbk2002320@gmail.com>
2021-03-22 01:05:30 +0900

    feat/fetch-data-date-count

--- src/pages/Userpage.tsx
+++ src/pages/Userpage.tsx
@@ -4,16 +4,17 @@ import { Link } from 'react-router-dom'
 import TextField from '@material-ui/core/TextField'
 import  { HeatmapCalendar }  from '../components/User'
 
-const Userpage: React.FC = () => {
-  const username = useAppSelector((state) => state.user.username)
 
+const Userpage: React.FC = () => {
+  const username = useAppSelector((state) => state.user.username)  
+  
   return (
     <div className='c-section-container'>
       <h2>マイページ</h2>
       <div className='module-spacer--small'></div>
       <TextField label='ユーザー名' value={username} />
       <div className="module-spacer--small"></div>
-      <HeatmapCalendar />
+      <HeatmapCalendar /*value={values}*/ />
       <div className="module-spacer--small"></div>
       <Link to='/todo' >todoに戻る</Link>
     </div>
