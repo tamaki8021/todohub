d9102822d5a8448ea858e473030dbf261d14db65
tamaki <hbk2002320@gmail.com>
2021-03-22 18:10:33 +0900

    feat/userpage-style

--- src/pages/Userpage.tsx
+++ src/pages/Userpage.tsx
@@ -1,24 +1,63 @@
-import React from 'react'
-import { useAppSelector } from '../reducks/store/hooks'
-import { Link } from 'react-router-dom'
-import TextField from '@material-ui/core/TextField'
-import  { HeatmapCalendar }  from '../components/User'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useAppSelector } from "../reducks/store/hooks";
+import { Link } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { AccountCircle } from "@material-ui/icons";
+import { HeatmapCalendar } from "../components/User";
 
+const useStyles = makeStyles({
+  center: {
+    margin: "0 auto",
+    width: 200,
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'white'
+  },
+  buttonCenter: {
+    margin: '0 auto',
+    width: 120
+  }
+});
 
 const Userpage: React.FC = () => {
-  const username = useAppSelector((state) => state.user.username)  
-  
-  return (
-    <div className='c-section-container'>
-      <h2>マイページ</h2>
-      <div className='module-spacer--small'></div>
-      <TextField label='ユーザー名' value={username} />
-      <div className="module-spacer--small"></div>
-      <HeatmapCalendar /*value={values}*/ />
-      <div className="module-spacer--small"></div>
-      <Link to='/todo' >todoに戻る</Link>
-    </div>
-  )
-}
+  const classes = useStyles();
+  const username = useAppSelector((state) => state.user.username);
 
-export default Userpage
+  return (
+    <div className="c-section-container">
+      <Typography variant="h5">マイページ</Typography>
+
+      <div className="module-spacer--small"></div>
+
+      <div className={classes.center}>
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <AccountCircle />
+          </Grid>
+          <Grid item>
+            <TextField id="input-with-icon-grid" label={username} />
+          </Grid>
+        </Grid>
+      </div>
+
+      <div className="module-spacer--small"></div>
+
+      <HeatmapCalendar />
+
+      <div className="module-spacer--small"></div>
+
+      <div className={classes.buttonCenter}>
+        <Button variant="contained" color="primary" >
+          <Link to="/todo" className={classes.link}>todoに戻る</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Userpage;
