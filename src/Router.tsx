880532334c6ccbe1814fcc94aac7e6b0a0922651
tamaki <hbk2002320@gmail.com>
2021-03-25 12:24:35 +0900

    feat/privacy-page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import {Home, SignUp, SignIn, Reset, Todo, Userpage } from './pages'
+import { Home, SignUp, SignIn, Reset, Todo, Userpage, Privacy } from './pages'
 import Auth from './Auth'
 
 const Router = () => {
@@ -10,6 +10,7 @@ const Router = () => {
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/signin/reset" component={Reset} />
+        <Route exact path="/privacy" component={Privacy} />
         <Auth>
           <Route exact path="/todo" component={Todo} />
           <Route exact path="/todo/mypage" component={Userpage} />
