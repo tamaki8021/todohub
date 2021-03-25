dab313db85f1a54120c3677f5a7b484cdbad9468
tamaki <hbk2002320@gmail.com>
2021-03-25 12:25:25 +0900

    braekpoint-privacy

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
-export {default as Home} from './Home'
-export {default as SignUp} from './SignUp'
-export {default as SignIn} from './SignIn'
-export {default as Reset} from './Reset'
-export {default as Todo} from './Todo'
-export {default as Userpage} from './Userpage'
+export { default as Home } from "./Home";
+export { default as SignUp } from "./SignUp";
+export { default as SignIn } from "./SignIn";
+export { default as Reset } from "./Reset";
+export { default as Todo } from "./Todo";
+export { default as Userpage } from "./Userpage";
+export { default as Privacy } from './Privacy'
